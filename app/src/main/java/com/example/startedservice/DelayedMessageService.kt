7e6e760e1d3463d23f58944eefd912901462b7a8
Sharath <sharathpavan7@gmail.com>
2019-08-07 00:26:57 +0530

    commit - v0.0.1 triggering notification insted of printing log

--- app/src/main/java/com/example/startedservice/DelayedMessageService.kt
+++ app/src/main/java/com/example/startedservice/DelayedMessageService.kt
@@ -1,7 +1,11 @@
 package com.example.startedservice
 
 import android.app.IntentService
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import java.lang.Exception
 
@@ -14,6 +18,7 @@ class DelayedMessageService : IntentService("DelayedMessageService") {
 
     companion object {
         val MESSAGE: String = "message"
+        val NOTIFICATION_ID = 100
     }
 
     val TAG: String = "DelayedMessageService"
@@ -28,6 +33,26 @@ class DelayedMessageService : IntentService("DelayedMessageService") {
     }
 
     fun showText(text: String) {
-        Log.i(TAG, text)
+        //Log.i(TAG, text)
+
+        // Intent to launch MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        // Pending Intent to pass to notification
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Notification builder configuring the notification
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(getString(R.string.question))
+            .setContentText(getString(R.string.answer))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(longArrayOf(0, 1000))
+            .setAutoCancel(true)
+
+        // NOtification manager to issue the notification
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
     }
 }
